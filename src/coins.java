4b83b7fab4f832de8b2c7bed0f5166dba0d028d7
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-08-06 21:32:51 -0500

    I hate dynamic programming

--- src/coins.java
+++ src/coins.java
@@ -4,8 +4,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class coins {
@@ -16,20 +16,67 @@ public class coins {
         Scanner scanner = new Scanner(inputFile);
         PrintWriter printWriter = new PrintWriter(outputFile);
 
-        while(scanner.hasNextLine()) {
+        while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
 
             if (line.equals("0")) {
                 break;
             }
 
-            Deque<Integer> coins = Stream.of(line)
+            int[] coins = Stream.of(line)
                     .map(s -> s.split(" "))
                     .flatMap(Arrays::stream)
-                    .map(Integer::valueOf)
-                    .collect(Collectors.toCollection(ArrayDeque::new));
+                    .skip(1)
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
 
-            System.out.println(coins);
+            int sum = Arrays.stream(coins).sum();
+
+//            int opt = findMaxValue(coins, 0, coins.length - 1, 0);
+
+            int valueChoosingLeft = findMaxValue(coins, 1, coins.length - 1, 0);
+            int valueChoosingRight = findMaxValue(coins, 0, coins.length - 2, 0);
+
+            int betterChoice = valueChoosingLeft > valueChoosingRight ? valueChoosingLeft : valueChoosingRight;
+
+            System.out.println(Arrays.toString(coins));
+            System.out.println(String.format("l: %s\nr: %s", valueChoosingLeft, valueChoosingRight));
+//            System.out.println(betterChoice);
+//            System.out.println(opt);
+        }
+    }
+
+    public static int findMaxValue(int[] coins, int startIndex, int endIndex, int acc) {
+//        System.out.println(String.format("start: %s, end: %s", startIndex, endIndex));
+        if (startIndex == endIndex || startIndex + 1 == endIndex) {
+            if (coins[startIndex] > coins[endIndex]) {
+                return acc + coins[startIndex];
+            } else {
+                return acc + coins[endIndex];
+            }
+        } else {
+            int bestMove = 0;
+
+            // cases
+
+
+            // I take left, they take right
+            int lr = findMaxValue(coins, startIndex + 1, endIndex - 1, acc + coins[startIndex]);
+            bestMove = lr > bestMove ? lr : bestMove;
+
+            // I take left, they take left
+            int ll = findMaxValue(coins, startIndex + 2, endIndex, acc + coins[startIndex]);
+            bestMove = ll > bestMove ? ll : bestMove;
+
+            // I take right, they take left
+            int rl = findMaxValue(coins, startIndex + 1, endIndex - 1, acc + coins[endIndex]);
+            bestMove = rl > bestMove ? rl : bestMove;
+
+            // I take right, they take right
+            int rr = findMaxValue(coins, startIndex, endIndex - 2, acc + coins[endIndex]);
+            bestMove = rr > bestMove ? rr : bestMove;
+
+            return bestMove;
         }
     }
 }
